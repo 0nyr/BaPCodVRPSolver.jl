4c792514884ba9f2708ebf554c608ccdbd615750
0nyr <rascoussier.florian@gmail.com>
2025-01-15 17:32:43 +0100

    Another attempt reusing old nixpkgs.

--- flake.nix
+++ flake.nix
@@ -1,40 +1,24 @@
 {
-  description = "Julia environment";
+  description = "Julia 1.5 environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:NixOS/nixpkgs/75a93dfecc6c77ed7c35cc7f906175aca93facb4"; # Pin to the specific commit
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        # Override the Nix package set to allow unfree packages
         pkgs = import nixpkgs {
-          system = system; 
-          config.allowUnfree = true; 
+          system = system;
+          config.allowUnfree = true; # Allow unfree packages if needed
         };
 
-        # WARN: Nix packaging system doesn't support all packages, so rely on Julia package manager instead.
-        # Use Julia in REPL mode, then package mode and install packages that way.
-        # WARN: Using a specific version of Julia to avoid compatibility issues.
-        julia_15 = pkgs.stdenv.mkDerivation {
-          name = "julia-1.5.4";
-          src = pkgs.fetchurl {
-            url = "https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.4-linux-x86_64.tar.gz";
-            sha256 = "sha256-gN7DUdGlk+itFSY2lxpI0Mgb/Pq5LIfzYEZjYW8ei8U=";
-          };
-          installPhase = ''
-            mkdir -p $out
-            cp -r * $out
-          '';
-        };
-      in
-      {
-        # development environment
+        julia_15 = pkgs.julia_15.overrideDerivation (oldAttrs: { doInstallCheck = false; });
+      in {
         devShells.default = pkgs.mkShell {
           packages = [
-            julia_15
+            julia_15 # Use Julia 1.5.4
           ];
 
           shellHook = ''
@@ -42,9 +26,8 @@
             export JULIA_PROJECT="turing"
             export JULIA_BINDIR=${julia_15}/bin
             export JULIA_EDITOR="code"
-            echo "Nix shell loaded."
+            echo "Julia 1.5 Nix shell loaded."
           '';
         };
-      }
-    );
+      });
 }
