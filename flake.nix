5f845c52df606a09ac8447409405676b438e3ff7
0nyr <rascoussier.florian@gmail.com>
2025-01-17 17:53:48 +0100

    Update to flake for BaPCoD compilation.

--- flake.nix
+++ flake.nix
@@ -2,23 +2,39 @@
   description = "Julia 1.5 environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/75a93dfecc6c77ed7c35cc7f906175aca93facb4"; # Pin to the specific commit
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+
+    nixpkgs-julia-15.url = "github:NixOS/nixpkgs/75a93dfecc6c77ed7c35cc7f906175aca93facb4"; # Pin to the specific commit
+
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, nixpkgs-julia-15, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        overlay-nixpkgs-julia-15 = final: prev: {
+          # Use the non-flake store path
+          pkgs-julia-15 = import nixpkgs-julia-15 { inherit system; };
+        };
+
         pkgs = import nixpkgs {
           system = system;
           config.allowUnfree = true; # Allow unfree packages if needed
+          overlays = [ overlay-nixpkgs-julia-15 ];
         };
 
-        julia_15 = pkgs.julia_15.overrideDerivation (oldAttrs: { doInstallCheck = false; });
+        julia_15 = pkgs.pkgs-julia-15.julia_15.overrideDerivation (oldAttrs: { doInstallCheck = false; });
       in {
         devShells.default = pkgs.mkShell {
           packages = [
-            julia_15 # Use Julia 1.5.4
+            julia_15 # Use specific Julia 1.5.4
+
+            pkgs.cmake
+            pkgs.gcc
+            pkgs.gdb
+            pkgs.zlib
+            pkgs.unzip
+
           ];
 
           shellHook = ''
@@ -27,6 +43,10 @@
             export JULIA_BINDIR=${julia_15}/bin
             export JULIA_EDITOR="code"
             alias julia=${julia_15}/bin/julia
+            export LD_LIBRARY_PATH=/home/onyr/cplex1210/cplex/bin/x86-64_linux:$LD_LIBRARY_PATH
+            export CPLEX_ROOT=/home/onyr/cplex1210
+            export BAPCOD_ROOT=/home/onyr/bapcod/bapcod-0.82.8
+            export BOOST_ROOT=/home/onyr/bapcod/bapcod-0.82.8/Tools/boost_1_76_0/build
             echo "Julia 1.5 Nix shell loaded."
           '';
         };
